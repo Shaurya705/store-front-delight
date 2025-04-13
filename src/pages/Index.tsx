56ce81b3e644e883816110951f9024779aec7db7
Shaurya705 <shauryajain057@gmail.com>
2025-04-13 11:26:09 +0530

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,7 +14,7 @@ const Index = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center">
         <div className="w-12 h-12 border-4 border-t-shop-blue border-gray-200 rounded-full animate-spin mx-auto"></div>
-        <p className="mt-4 text-shop-text">Loading...</p>
+        <p className="mt-4 text-shop-text">Loading..</p>
       </div>
     </div>
   );
